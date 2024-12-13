250dbd11f2343d9cc0934cd1d44d6ba4ec484506
Hire Me. <98966754+Nwanna-Joseph@users.noreply.github.com>
2024-12-13 21:14:01 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,18 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    },
+  ],
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
