558f9004633501a504d6e7991c6765f9971965a8
Hire Me. <98966754+Nwanna-Joseph@users.noreply.github.com>
2024-12-14 21:39:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 function component() {
-  const element = document.createElement("div");
-  element.innerHTML = "Hello World";
-  return element;
+  // const element = document.createElement("div");
+  // element.innerHTML = "Hello World";
+  // return element;
+  alert(0)
 }
 
 document.body.appendChild(component());
